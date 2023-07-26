5a2296ffa291919616854264f1c12414d3a3bcab
kyleung1 <leungkyle2@gmail.com>
2023-07-26 02:23:34 -0700

    asdf

--- app/Project_Preview.tsx
+++ app/Project_Preview.tsx
@@ -28,7 +28,7 @@ export default function Project_Preview() {
               <h1 className='text-2xl text-black text-center my-3 font-serif'>Personal Website</h1>
               <img className="transform transition-all duration-300 hover:scale-110 my-3" src="/personal-site.PNG" alt="personal site"/>
               <p className='text-black my-3'>Next.js, Tailwind, Node.js, TypeScript</p>
-              <p>A portfolio website to showcase my skills and projects. I am planning on regularly updating this site as I build more projects in the future.</p>
+              <p> A portfolio website to showcase my skills and projects. I am planning on regularly updating this site as I build more projects in the future. </p>
               <div className='flex justify-end'>
                 <div className='w-10' onMouseEnter={() => {sethideGit1(false)}} onMouseLeave={() => {sethideGit1(true)}}>
                   {hideGit1 && (
@@ -57,7 +57,7 @@ export default function Project_Preview() {
               <h1 className='text-2xl text-black text-center my-3 font-serif'>Package Tracker</h1>
               <Link href="https://packagetracker.tech/"><img className="transform transition-all duration-300 hover:scale-110 my-3" src="/package-tracker.PNG" alt="package tracker"/></Link>
               <p className='text-black my-3'>Next.js, Firebase, Node.js, TypeScript</p>
-              <p>A web app that tracks locations of packages in real time and their status. This project was made for Transport Hacks and won first place.</p>
+              {/* <p>A web app that tracks locations of packages in real time and their status. This project was made for Transport Hacks and won first place.</p> */}
               <div className='flex justify-end'>
                 <div className='w-10' onMouseEnter={() => {sethideGit2(false)}} onMouseLeave={() => {sethideGit2(true)}}>
                   {hideGit2 && (
@@ -85,7 +85,7 @@ export default function Project_Preview() {
               <h1 className='text-2xl text-black text-center my-3 font-serif'>Replayed</h1>
               <Link href="https://replayedgg.vercel.app/"><img className="transform transition-all duration-300 hover:scale-110 my-3" src="/replayed.PNG" alt="replayed"/></Link>
               <p className='text-black my-3'>Svelte, Tailwind, TypeScript, Chart.js</p>
-              <p>A web game that challenges players to guess the rank of a player based on a short video. The game features videos of games such as League of Legends, Valorant, and Overwatch2. There are two game modes: traditional where the user guesses one video and duel where the user guesses between 2 videos who has the higher rank.</p>
+              {/* <p>A web game that challenges players to guess the rank of a player based on a short video. The game features videos of games such as League of Legends, Valorant, and Overwatch2. There are two game modes: traditional where the user guesses one video and duel where the user guesses between 2 videos who has the higher rank.</p> */}
               <div className='flex justify-end'>
                 <div className='w-10' onMouseEnter={() => {sethideGit3(false)}} onMouseLeave={() => {sethideGit3(true)}}>
                   {hideGit3 && (
@@ -113,7 +113,7 @@ export default function Project_Preview() {
               <h1 className='text-2xl text-black text-center my-3 font-serif'>Web Dev Evaluator</h1>
               <Link href="https://webdev-evaluator.vercel.app/"><img className="transform transition-all duration-300 hover:scale-110 my-3" src="/webdeveval.PNG" alt="web dev evaluator"/></Link>
               <p className='text-black my-3'>Next.js, TypeScript, Tailwind, Rust, MongoDB</p>
-              <p>A website that provides sentiment and statistics about each web development technology in 2022. Gathered and analyzed a dataset of over 48,000 tweets.</p>
+              {/* <p>A website that provides sentiment and statistics about each web development technology in 2022. Gathered and analyzed a dataset of over 48,000 tweets.</p> */}
               <div className='flex justify-end'>
                 <div className='w-10' onMouseEnter={() => {sethideGit4(false)}} onMouseLeave={() => {sethideGit4(true)}}>
                   {hideGit4 && (
@@ -141,7 +141,7 @@ export default function Project_Preview() {
               <h1 className='text-2xl text-black text-center my-3 font-serif'>Seattle Crime Locator</h1>
               <Link href="https://kyleung1.newGithub.io/SeattleCrimeLocator/"><img className="transform transition-all duration-300 hover:scale-110 my-3" src="/crimelocator.PNG" alt="seattle crime locator"/></Link>
               <p className='text-black my-3'>JavaScript, HTML, CSS, OpenLayers</p>
-              <p>A simple web application to locate the nearest crime from either the user's ip address or a given address. The web app sources it's data from the spd public dataset on the Seattle Open Data portal that is updated daily. This was my first ever web application built and I am proud of my progress since.</p>
+              {/* <p>A simple web application to locate the nearest crime from either the user's ip address or a given address. The web app sources it's data from the spd public dataset on the Seattle Open Data portal that is updated daily. This was my first ever web application built and I am proud of my progress since.</p> */}
               <div className='flex justify-end'>
                 <div className='w-10' onMouseEnter={() => {sethideGit5(false)}} onMouseLeave={() => {sethideGit5(true)}}>
                   {hideGit5 && (
