6f6bd02f38b8129e71fba2c455ad904fa967de67
kyleung1 <92060005+kyleung1@users.noreply.github.com>
2023-08-13 20:27:57 -0700

    chatbot

---
+++ app/Chatbot/page.tsx
@@ -0,0 +1,14 @@
+
+export default function page() {
+    return (
+      <>
+        <h1 className="text-indigo-800 text-5xl text-center my-10">Chat-KL1</h1>
+        <div className="flex flex-col items-center">
+            <div className="border-2 border-indigo-800 rounded-md max-w-[1280px] w-3/4 h-[500px] my-10">
+                <div className="">hi</div>
+                <input placeholder="Type something"/>
+            </div>
+        </div>
+      </>
+    )
+  }
