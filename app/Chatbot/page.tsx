0f4dc271161c99112edea560ce4f5919b27e915e
kyleung1 <leungkyle2@gmail.com>
2023-08-14 20:38:30 -0700

    a

--- app/Chatbot/page.tsx
+++ app/Chatbot/page.tsx
@@ -59,7 +59,7 @@ export default function Page() {
         <h1 className="text-indigo-800 text-5xl text-center my-10">Chat-KL1</h1>
         <div className="flex flex-col items-center">
             <div className="flex border-2 border-black rounded-md max-w-[1280px] w-3/4 h-[500px] my-10 relative">
-                <div className="flex-grow">hi</div>
+                <div className="flex-grow overflow-scroll">hi</div>
                 <div className="absolute bottom-0 w-full bg-violet-300 p-2 flex justify-around border-t-2 border-black">
                   <input ref={user_input} placeholder="Type something" className="w-3/4 bg-violet-300"/>
                   <div className='w-10' onMouseEnter={() => {sethideSend(false)}} onMouseLeave={() => {sethideSend(true)}}>
