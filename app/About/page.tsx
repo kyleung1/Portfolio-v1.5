708b89e01e99c6be08694ebf267fe263f80b1726
kyleung1 <92060005+kyleung1@users.noreply.github.com>
2023-08-15 00:30:27 -0700

    chatbot

--- app/About/page.tsx
+++ app/About/page.tsx
@@ -1,8 +1,11 @@
+import Link from "next/link";
+
 export default function page() {
     return (
       <div className="flex flex-col items-center">
         <h1 className="text-5xl my-20 text-indigo-800">About Me</h1>
         <img src="/shinji.png" alt="shinji"/>
+        <Link href="/Chatbot" className="hover:text-rose-500 my-10">Learn more about me through my Chatbot</Link>
       </div>
     )
   }
--- app/Chatbot/page.tsx
+++ app/Chatbot/page.tsx
@@ -17,17 +17,29 @@ interface requestBody {
 
 export default function Page() {
     const [hideSend, sethideSend] = useState(true); // toggles button hover effeect
-    const [session, setSession] = useState(-1);
+    const [session, setSession] = useState <number> ();
+    const [messages, setMessages] = useState<string[]>([])
 
+    const chatWindow: RefObject<HTMLDivElement> = useRef(null);
     const user_input: RefObject<HTMLInputElement> = useRef(null);
 
+    useEffect(() => {
+      // scroll to the bottom when messages fill up
+      if (chatWindow.current)
+      chatWindow.current.scrollTop = chatWindow.current?.scrollHeight;
+    }, [messages])
+
     async function getResponse() {
+      let user_msg = user_input.current?.value;
+      if (user_msg){
+        addMessage(user_msg);
+      }
 
       let requestBody: requestBody = {
-        user_msg: user_input.current?.value
+        user_msg: user_msg
       };
 
-      if (session !== -1) {
+      if (session !== null) {
         requestBody.session = session
       };
 
@@ -43,31 +55,46 @@ export default function Page() {
     };
 
     function handleResponse(response: ChatResponse) {
-      console.log(response)
       if (session !== response.session) {
         setSession(response.session);
       };
       const chatbotReply = response.answer;
+      addMessage(chatbotReply);
+      if (user_input.current){
+        user_input.current.value = "";
+      }
     };
 
-    function a () {
-      console.log(user_input.current?.value)
-    }
+    function addMessage(newMsg: string) {
+      setMessages(prevMsgs => [...prevMsgs, newMsg]);
+    };
 
     return (
       <>
         <h1 className="text-indigo-800 text-5xl text-center my-10">Chat-KL1</h1>
         <div className="flex flex-col items-center">
-            <div className="flex border-2 border-black rounded-md max-w-[1280px] w-3/4 h-[500px] my-10 relative">
-                <div className="flex-grow overflow-scroll">hi</div>
-                <div className="absolute bottom-0 w-full bg-violet-300 p-2 flex justify-around border-t-2 border-black">
-                  <input ref={user_input} placeholder="Type something" className="w-3/4 bg-violet-300"/>
+            <div className="flex flex-col border-2 border-black rounded-md max-w-[1280px] w-3/4 h-[500px] my-10 relative">
+                <div ref={chatWindow} className="flex-grow overflow-scroll">
+                  {messages.map((message, index) => (
+                    <div key={index} className={`${index % 2 === 0 ? '' : 'text-indigo-500'} py-3 px-32 text-center border-b-2 border-neutral-300`}>{message}</div>
+                  ))}
+                </div>
+                <div className="w-full bg-violet-300 p-2 flex justify-around border-t-2 border-black">
+                  <input
+                    ref={user_input}
+                    placeholder="Type something"
+                    className="w-3/4 bg-violet-300"
+                    onKeyDown={(e) => {
+                      if (e.key === 'Enter') {
+                        getResponse()
+                      }}}
+                      />
                   <div className='w-10' onMouseEnter={() => {sethideSend(false)}} onMouseLeave={() => {sethideSend(true)}}>
                     {hideSend && (
                       <button><img src="/send-black.png" alt="newGithub" className='w-3/4'/></button>
                     )}
                     {hideSend === false && (
-                      <button onClick={a}><img src="/send-rose.png" alt="send button" className="w-3/4"/></button>
+                      <button onClick={getResponse}><img src="/send-rose.png" alt="send button" className="w-3/4"/></button>
                     )}
                   </div>
                 </div>
