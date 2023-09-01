4bc9139a8ab42c39558587678424c081675118d6
kyleung1 <leungkyle2@gmail.com>
2023-08-31 23:14:35 -0700

    resume update

--- app/Skills/page.tsx
+++ app/Skills/page.tsx
@@ -8,7 +8,7 @@ export default function page() {
         <div className="flex flex-col justify-center items-center ms mt-20">
           <h1 className="text-5xl text-indigo-800" data-aos="fade-right">Resume</h1>
           <Link href="https://kyleung1.github.io/Resume/" target="_blank" className="hover:text-rose-500 my-10">Click here to view my resume in a new window</Link>
-          <iframe src="/Resume_2023 (1).pdf" className="w-1/2 my-20 lg:h-[1200px] h-[500px]" data-aos="fade-right"></iframe>
+          <iframe src="/Resume_Aug_2023.pdf" className="w-1/2 my-20 lg:h-[900px] h-[500px]" data-aos="fade-right"></iframe>
         </div>
       </>
     )
